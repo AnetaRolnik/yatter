338f857a2c2657afe72aed1e8f04e555ba3765af
Aneta Rolnik <aneta.r@poczta.onet.eu>
2021-09-19 18:19:58 +0200

    Add validation to the login form

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
 
 class AuthForm extends StatefulWidget {
   const AuthForm({Key? key}) : super(key: key);
@@ -8,6 +9,20 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  String _userEmail = '';
+  String _userName = '';
+  String _userPassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState!.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      _formKey.currentState!.save();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -16,6 +31,7 @@ class _AuthFormState extends State<AuthForm> {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
           child: Form(
+            key: _formKey,
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
@@ -24,22 +40,49 @@ class _AuthFormState extends State<AuthForm> {
                   decoration: const InputDecoration(
                     labelText: 'Email address',
                   ),
+                  validator: (val) {
+                    if (EmailValidator.validate(val!) == false) {
+                      return 'Please enter a valid email address.';
+                    }
+                    return null;
+                  },
+                  onSaved: (val) {
+                    _userEmail = val!;
+                  },
                 ),
                 TextFormField(
                   decoration: const InputDecoration(
                     labelText: 'Username',
                   ),
+                  validator: (val) {
+                    if (val!.isEmpty || val.length < 4) {
+                      return 'Password enter at least 4 characters.';
+                    }
+                    return null;
+                  },
+                  onSaved: (val) {
+                    _userName = val!;
+                  },
                 ),
                 TextFormField(
                   decoration: const InputDecoration(
                     labelText: 'Password',
                   ),
                   obscureText: true,
+                  validator: (val) {
+                    if (val!.isEmpty || val.length < 6) {
+                      return 'Password must be at least 6 characters long.';
+                    }
+                    return null;
+                  },
+                  onSaved: (val) {
+                    _userPassword = val!;
+                  },
                 ),
                 const SizedBox(height: 12),
                 ElevatedButton(
                   child: const Text('Log in'),
-                  onPressed: () {},
+                  onPressed: _trySubmit,
                 ),
                 TextButton(
                   child: const Text("Don't have an account yet?' Sign up"),
