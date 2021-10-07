8c9f3c66e4c06ec786bb0390e0972c59251e5efb
Aneta Rolnik <aneta.r@poczta.onet.eu>
2021-10-07 19:42:14 +0200

    Add a logout feature

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -9,6 +10,31 @@ class ChatScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Yatter'),
+        actions: <Widget>[
+          DropdownButton(
+            icon: const Icon(Icons.more_vert, color: Colors.white),
+            items: [
+              DropdownMenuItem(
+                child: Row(
+                  children: const <Widget>[
+                    Icon(
+                      Icons.exit_to_app,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(width: 8),
+                    Text('Logout'),
+                  ],
+                ),
+                value: 'logout',
+              )
+            ],
+            onChanged: (itemIdentifier) {
+              if (itemIdentifier == 'logout') {
+                FirebaseAuth.instance.signOut();
+              }
+            },
+          )
+        ],
       ),
       body: StreamBuilder<QuerySnapshot>(
         stream: FirebaseFirestore.instance
