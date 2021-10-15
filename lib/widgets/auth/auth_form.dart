a0bae179ac4afcae75fc814827f550df4a914671
Aneta Rolnik <aneta.r@poczta.onet.eu>
2021-10-15 11:15:15 +0200

    Add animation to authorization form

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -66,80 +66,90 @@ class _AuthFormState extends State<AuthForm> {
         margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                if (!_isLogin) UserImagePicker(_pickedImage),
-                TextFormField(
-                  key: const ValueKey('email'),
-                  autocorrect: false,
-                  textCapitalization: TextCapitalization.none,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                    labelText: 'Email address',
-                  ),
-                  validator: (val) {
-                    if (EmailValidator.validate(val!) == false) {
-                      return 'Please enter a valid email address.';
-                    }
-                    return null;
-                  },
-                  onSaved: (val) {
-                    _userEmail = val!;
-                  },
-                ),
-                if (!_isLogin)
-                  TextFormField(
-                    key: const ValueKey('username'),
-                    decoration: const InputDecoration(
-                      labelText: 'Username',
-                    ),
-                    validator: (val) {
-                      if (val!.isEmpty || val.length < 4) {
-                        return 'Password enter at least 4 characters.';
-                      }
-                      return null;
-                    },
-                    onSaved: (val) {
-                      _userName = val!;
-                    },
-                  ),
-                TextFormField(
-                  key: const ValueKey('password'),
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                  ),
-                  obscureText: true,
-                  validator: (val) {
-                    if (val!.isEmpty || val.length < 6) {
-                      return 'Password must be at least 6 characters long.';
-                    }
-                    return null;
-                  },
-                  onSaved: (val) {
-                    _userPassword = val!;
-                  },
-                ),
-                const SizedBox(height: 12),
-                widget.isLoading
-                    ? const CircularProgressIndicator()
-                    : ElevatedButton(
-                        child: Text(_isLogin ? 'Log in' : 'Sign up'),
-                        onPressed: _trySubmit,
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 350),
+            curve: Curves.easeIn,
+            constraints: BoxConstraints(
+              minHeight: _isLogin ? 224 : 390,
+              maxHeight: _isLogin ? 268 : 456,
+            ),
+            child: Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    if (!_isLogin) UserImagePicker(_pickedImage),
+                    TextFormField(
+                      key: const ValueKey('email'),
+                      autocorrect: false,
+                      textCapitalization: TextCapitalization.none,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        labelText: 'Email address',
                       ),
-                TextButton(
-                  child: Text(_isLogin
-                      ? 'Create new account'
-                      : 'I already have an account'),
-                  onPressed: () {
-                    setState(() {
-                      _isLogin = !_isLogin;
-                    });
-                  },
+                      validator: (val) {
+                        if (EmailValidator.validate(val!) == false) {
+                          return 'Please enter a valid email address.';
+                        }
+                        return null;
+                      },
+                      onSaved: (val) {
+                        _userEmail = val!;
+                      },
+                    ),
+                    if (!_isLogin)
+                      TextFormField(
+                        key: const ValueKey('username'),
+                        decoration: const InputDecoration(
+                          labelText: 'Username',
+                        ),
+                        validator: (val) {
+                          if (val!.isEmpty || val.length < 4) {
+                            return 'Password enter at least 4 characters.';
+                          }
+                          return null;
+                        },
+                        onSaved: (val) {
+                          _userName = val!;
+                        },
+                      ),
+                    TextFormField(
+                      key: const ValueKey('password'),
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                      ),
+                      obscureText: true,
+                      validator: (val) {
+                        if (val!.isEmpty || val.length < 6) {
+                          return 'Password must be at least 6 characters long.';
+                        }
+                        return null;
+                      },
+                      onSaved: (val) {
+                        _userPassword = val!;
+                      },
+                    ),
+                    const SizedBox(height: 12),
+                    widget.isLoading
+                        ? const CircularProgressIndicator()
+                        : ElevatedButton(
+                            child: Text(_isLogin ? 'Log in' : 'Sign up'),
+                            onPressed: _trySubmit,
+                          ),
+                    TextButton(
+                      child: Text(_isLogin
+                          ? 'Create new account'
+                          : 'I already have an account'),
+                      onPressed: () {
+                        setState(() {
+                          _isLogin = !_isLogin;
+                        });
+                      },
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
